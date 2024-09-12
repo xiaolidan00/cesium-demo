7669188c5cc7ffcc09edb632e6a78bcdaf031872
xiaolidan00 <764937567@qq.com>
2024-09-13 00:53:17 +0800

    feat:urls

---
+++ urls.js
@@ -0,0 +1,15 @@
+const urls = [
+  {
+    title: '自定义Primitive',
+    entry: 'index.ts',
+    filename: `src/customPrimitive/index.html`,
+    template: `src/customPrimitive/index.html`,
+    injectOptions: {
+      data: {
+        title: '自定义Primitive'
+      }
+    }
+  }
+];
+
+export default urls;
